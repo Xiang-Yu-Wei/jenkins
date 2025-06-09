e8ffe349f08cbce4735e61e2992c3a036d0bc9b3
xiangyu.wei <xiangyu.wei@Rakuten.com>
2025-06-09 16:22:28 +0800

    1

--- vars/assignGlobleRoles.groovy
+++ vars/assignGlobleRoles.groovy
@@ -1,6 +1,27 @@
+import com.michelin.cio.hudson.plugins.rolestrategy.*
+import hudson.security.Permission
+import jenkins.model.Jenkins
+import com.synopsys.arc.jenkins.plugins.rolestrategy.RoleType
+
 def call(List<String> userList = null, List<String> groupList = null ,String roleName = null) {
-    def role = new org.test.assignRoles.assignGlobleRoles()
-    role.assignUsersToGlobleRole(userList, groupList ,roleName)
+
+    Jenkins jenkins = Jenkins.get()
+    def rbas = jenkins.getAuthorizationStrategy()
+    Set<Permission> permissions = new HashSet<>()
+    permissions.add(Jenkins.ADMINISTER)
+    Role adminRole = new Role(roleName, permissions)
+    def globalRoleMap = rbas.getRoleMap(RoleType.Global)
+
+    userList?.each { username ->
+        globalRoleMap.assignRole(adminRole, new PermissionEntry(AuthorizationType.USER, username))
+    }
+    groupList?.each { groupname ->
+        globalRoleMap.assignRole(adminRole,new PermissionEntry(AuthorizationType.GROUP, groupname))
+    }
+
+    jenkins.setAuthorizationStrategy(rbas)
+    jenkins.save()
+
 
 
 }
